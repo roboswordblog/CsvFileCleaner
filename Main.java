5f335705da71cbc6a335ae00d3ee2e14d674c5f6
Robosword <32asingh@stratfordschools.net>
2026-04-24 22:52:21 -0700

    Add the text field for putting in the file name. W CONSOLAS FONT

--- Main.java
+++ Main.java
@@ -9,7 +9,15 @@ public class Main {
         window.setLocationRelativeTo(null);
         window.setResizable(false);
         window.setLayout(null);
-
+        JTextField fileName = new JTextField("example.csv");
+        fileName.setSize(150, 50);
+        fileName.setLocation(50, 5);
+        fileName.setBackground(new Color(50, 50, 50));
+        fileName.setForeground(Color.WHITE);
+        fileName.setCaretColor(Color.WHITE);
+        fileName.setBorder(BorderFactory.createLineBorder(new Color(70, 70, 70)));
+        fileName.setFont(new Font("Consolas", Font.PLAIN, 14));
+        window.add(fileName);
 
         window.getContentPane().setBackground(new Color(30, 30, 30));
         window.setVisible(true);
