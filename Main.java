3da8dcfdb124ca6e3a7148f78f9844e17028e392
Robosword <32asingh@stratfordschools.net>
2026-04-24 22:46:13 -0700

    Resise the window and make it visible

--- Main.java
+++ Main.java
@@ -5,9 +5,10 @@ public class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame("CSV FILE CLEANER");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(800, 600);
+        window.setSize(900, 700);
         window.setLocationRelativeTo(null);
         window.setResizable(false);
         window.setLayout(null);
+        window.setVisible(true);
     }
 }
