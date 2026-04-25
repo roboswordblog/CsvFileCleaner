160f3d5b410f3d0a9b0e3bb08eb1d6a29a8193a7
Robosword <32asingh@stratfordschools.net>
2026-04-24 22:51:44 -0700

    Resize the window again, and also set the background color to a different color(W darktheme)

--- Main.java
+++ Main.java
@@ -9,6 +9,8 @@ public class Main {
         window.setLocationRelativeTo(null);
         window.setResizable(false);
         window.setLayout(null);
+
+
         window.getContentPane().setBackground(new Color(30, 30, 30));
         window.setVisible(true);
     }
