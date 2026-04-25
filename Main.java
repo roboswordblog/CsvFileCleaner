bfe82ba7b5cd33fad8343b997198db660c0e4c6c
Robosword <32asingh@stratfordschools.net>
2026-04-24 22:48:30 -0700

    Resize the window again, and also set the background color to a different color(W darktheme)

--- Main.java
+++ Main.java
@@ -5,10 +5,11 @@ public class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame("CSV FILE CLEANER");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(900, 700);
+        window.setSize(1100, 675);
         window.setLocationRelativeTo(null);
         window.setResizable(false);
         window.setLayout(null);
+        window.getContentPane().setBackground(new Color(30, 30, 30));
         window.setVisible(true);
     }
 }
