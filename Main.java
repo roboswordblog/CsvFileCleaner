b5f74254891e12a21e1874245e831a6a1f4d4ea0
Robosword <32asingh@stratfordschools.net>
2026-04-24 22:59:13 -0700

    Add the load button for the file.

--- Main.java
+++ Main.java
@@ -9,6 +9,7 @@ public class Main {
         window.setLocationRelativeTo(null);
         window.setResizable(false);
         window.setLayout(null);
+
         JTextField fileName = new JTextField("example.csv");
         fileName.setSize(150, 50);
         fileName.setLocation(50, 5);
@@ -19,6 +20,17 @@ public class Main {
         fileName.setFont(new Font("Consolas", Font.PLAIN, 14));
         window.add(fileName);
 
+        JButton loadButton = new JButton("Load");
+        loadButton.setBounds(210, 10, 100, 40);
+        loadButton.setBackground(new Color(45, 45, 45));
+        loadButton.setForeground(Color.WHITE);
+        loadButton.setFocusPainted(false);
+        loadButton.setBorder(BorderFactory.createLineBorder(new Color(70, 70, 70)));
+        loadButton.setFont(new Font("Consolas", Font.BOLD, 14));
+        loadButton.setOpaque(true);
+        loadButton.setContentAreaFilled(true);
+
+        window.add(loadButton);
         window.getContentPane().setBackground(new Color(30, 30, 30));
         window.setVisible(true);
     }
