df79721f7cb0acbbe3145f02c666425368b87c55
Robosword <32asingh@stratfordschools.net>
2026-04-24 22:44:40 -0700

    Create the window for the application.

--- Main.java
+++ Main.java
@@ -3,7 +3,11 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        JFrame window = new JFrame("CSV FILE CLEANER");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(800, 600);
+        window.setLocationRelativeTo(null);
+        window.setResizable(false);
+        window.setLayout(null);
     }
 }
