ef8f0e34893a55e7da9dba3417cc72fa9127c52f
Robosword <32asingh@stratfordschools.net>
2026-04-25 18:15:09 -0700

    Nested loops to assign each one to a column

--- dataHandling.java
+++ dataHandling.java
@@ -10,7 +10,12 @@ public class dataHandling{
         file = files;
         String content = Files.readString(Path.of(files));
         String[] lines = content.split("\n");
+        for (int i=0;i<lines[0].length();i++){
 
+            for (int j=1;j<lines.length;j++){
+
+            }
+        }
         return fileData;
     }
 
