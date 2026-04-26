a191014e08e5b016085e7ba5f40d73197b53c7ff
Robosword <32asingh@stratfordschools.net>
2026-04-25 17:33:05 -0700

    Get the content of whatever file the user chooses.

--- dataHandling.java
+++ dataHandling.java
@@ -1,11 +1,15 @@
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Path;
 public class dataHandling{
     String file;
     HashMap<String, ArrayList<String>> fileData;
 
-    HashMap<String, ArrayList<String>> load(String files){
+    HashMap<String, ArrayList<String>> load(String files) throws IOException {
         file = files;
+        String content = Files.readString(Path.of(files));
         return fileData;
     }
 
