79768a0e3fff5e3fb9bb14de373074fe30afd9e9
Robosword <32asingh@stratfordschools.net>
2026-04-25 17:47:09 -0700

    Seperate the content into lines.

--- dataHandling.java
+++ dataHandling.java
@@ -1,15 +1,16 @@
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.ArrayList;
 import java.nio.file.Files;
 import java.nio.file.Path;
 public class dataHandling{
     String file;
-    HashMap<String, ArrayList<String>> fileData;
+    HashMap<String, String[]> fileData;
 
-    HashMap<String, ArrayList<String>> load(String files) throws IOException {
+    HashMap<String,String[]> load(String files) throws IOException {
         file = files;
         String content = Files.readString(Path.of(files));
+        String[] lines = content.split("\n");
+
         return fileData;
     }
 
