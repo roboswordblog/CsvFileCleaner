4dafd96ddde6b89bcd6ec6184a774b648e03f8aa
Robosword <32asingh@stratfordschools.net>
2026-04-25 18:35:55 -0700

    Restart because of differences between JTable and hashmap(going to use listarray and convert it to a list)

--- dataHandling.java
+++ dataHandling.java
@@ -5,25 +5,16 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 public class dataHandling{
     String file;
-    HashMap<String, ArrayList<String>> fileData = new HashMap<>();
+    ArrayList<ArrayList<ArrayList<String>>> fileData = new ArrayList<>();
 
-    HashMap<String,ArrayList<String>> load(String files) throws IOException {
+    ArrayList<ArrayList<ArrayList<String>>> load(String files) throws IOException {
 
         fileData.clear();
         file = files;
         String content = Files.readString(Path.of(files));
         String[] lines = content.split("\\r?\\n");
         for (int i=0;i<lines[0].split(",").length;i++){
-            ArrayList<String> thingies = new ArrayList<>();
-            for (int j=1;j<lines.length;j++){
-                String[] stuff = lines[j].split(",");
-                if (i < stuff.length) {
-                    thingies.add(stuff[i]);
-                } else {
-                    thingies.add("");
-                }
-            }
-            fileData.put(lines[0].split(",")[i], thingies);
+
         }
         return fileData;
     }
